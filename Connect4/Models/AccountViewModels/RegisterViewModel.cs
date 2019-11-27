9fc9ca98eb60a4c1951bbb0929951b798e11713a
Jociel Barbosa <jocieltb@gmail.com>
2019-11-27 20:22:44 -0300

    Validação CPF e CEP e msg err

--- Connect4/Models/AccountViewModels/RegisterViewModel.cs
+++ Connect4/Models/AccountViewModels/RegisterViewModel.cs
@@ -15,7 +15,7 @@ namespace Connect4.Models.AccountViewModels
         public string Nome { get; set; }
 
         [Required]
-        [MinLength(11), MaxLength(11)]
+        [Range(11, 11, ErrorMessage = "CPF Invalido, necessário 11 digitos")]
         [Display(Name = "CPF")]
         public string CPF { get; set; }
 
@@ -25,7 +25,7 @@ namespace Connect4.Models.AccountViewModels
 
         [Required]
         [Display(Name = "CEP")]
-        [MinLength(8), MaxLength(8)]
+        [Range(8, 8, ErrorMessage = "CEP Invalido, necessário 8 digitos")]
         public string CEP { get; set; }
 
         [Required]
